dcb4b3f4dc4b4ebb8e38de784748a61e8765c6c6
K.V. <kvonwin@gmail.com>
2024-12-09 01:10:36 -0600

    Update gh pages

---
+++ fakeTa.js
@@ -0,0 +1,23 @@
+var loc = "home";
+var locations = [
+  "home",
+  "wiki index",
+  "links"
+];
+
+function look(){
+  console.log('You are at ' + window.location.href + '.');
+}
+
+function inventory(){
+  console.log('You aren't carrying anything.');
+}
+
+function jump(){
+  console.log('You jump in place.');
+}
+
+function xyzzy(){
+  console.log('Surprisingly, nothing happens.');
+}
+
